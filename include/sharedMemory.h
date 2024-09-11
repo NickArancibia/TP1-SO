502d0f3a910a7c774d649c8828d6fcedb9d3459b
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2024-09-11 00:18:38 -0300

    feat- common functions for shared memory

--- include/sharedMemory.h
+++ include/sharedMemory.h
@@ -7,9 +7,16 @@
 #include <sys/mman.h>
 #include "definitions.h"
 
+
 typedef struct {
     char md5[MD5LEN+1], filename[MAXFILELEN+1];
     pid_t pid;
 } message;
+#include "vistaLib.h"
+
+void *createSharedMemory(char* name, int size, int* fd);
+void *openSharedMemory(char* name, int size, int* fd);
+void closeSharedMemory(void *shm_ptr, int size,int fd);
+void destroySharedMemory(const char *name, void *shm_ptr, int fd,int size);
 
 #endif 
