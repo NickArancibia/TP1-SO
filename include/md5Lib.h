464a8f91cf71a8fcce01831609988d63a72b2218
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2024-09-16 15:43:49 -0300

    code refactored

--- include/md5Lib.h
+++ include/md5Lib.h
@@ -12,6 +12,7 @@
 #define ERROR -1
 #define R_END 0
 #define W_END 1
+#define DEFAULT_INITIAL_LOAD 1
 
 int createChildsAndPipes(int childsQty,int  md5SendData[][2],int slaveSendData[][2],int * pids);
 void sendData(int fd,char const *message[],int* dataLeft,int* idx,int qty);
